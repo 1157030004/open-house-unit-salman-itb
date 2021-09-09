370c69b20e7e4435e393d31601eaa9d823cba381
1157030004 <1157030004@student.uinsgd.ac.id>
2021-09-09 14:11:47 +0700

    change router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Home from "./pages/home/Home";
 import Single from "./pages/unit/Single";
